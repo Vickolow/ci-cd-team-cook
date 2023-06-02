8de7bbaeee808aa4dbc5ef62b0d00daf5642cb58
Vickolow <ainavictorkolawole@gmail.com>
2023-06-02 15:39:10 +0100

    add unit test

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
-
 import main
 import unittest
 
-class TestMain(unittest.Testcase)
+class IntegerArithmeticTestCase(unittest.TestCase):
+        def testAdd(self):  
+        # test method names begin with 'test'
 
-    def test_addition(self):
-        self.assertEqual(main.addition(1, 2), 3,"Should be 3")
-        self.assertEqual(main.addition(5, 5), 10,"Should be 10")
-        self.assertEqual(main.addition(40, 20), 3,"Should be 60")
-        self.assertEqual(main.addition(-3, 2), -1,"Should be -1") 
+            self.assertEqual((1 + 2), 3)
+            self.assertEqual(0 + 1, 1)
 
-    if __name__ == '__main__':
-        unittest.main()       
+if __name__ == '__main__':
+        unittest.main()
