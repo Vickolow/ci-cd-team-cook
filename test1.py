49d7e92517fc20536d8388d233a2af437b3ed70b
Vickolow <ainavictorkolawole@gmail.com>
2023-06-10 12:00:41 +0100

    added a new line

--- test1.py
+++ test1.py
@@ -9,6 +9,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(main.addition(5, 5), 10,"Should be 10")
         self.assertEqual(main.addition(40, 20), 3,"Should be 60")
         self.assertEqual(main.addition(-3, 2), -1,"Should be -1") 
+        
 
     if __name__ == '__main__':
         unittest.main()       
